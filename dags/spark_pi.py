29f7c72788f4bc75c024a0641c5ed02f56876533
George Alhajje <mgeorge-ha@hotmail.com>
2025-05-27 22:10:18 +0200

    Add a descriptive commit message

--- dags/spark_pi.py
+++ dags/spark_pi.py
@@ -40,7 +40,7 @@ submit = SparkKubernetesOperator(
     task_id='spark_transform_data',
     namespace='spark-operator',
     application_file='/kubernetes/spark-pi.yaml',
-    kubernetes_conn_id='kubernetes_default',
+    kubernetes_conn_id='myk8s',
     do_xcom_push=True,
 )
 
